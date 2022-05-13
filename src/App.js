b1cb84a09cedce5deb305266ea460df09e8be5c1
Matheus Chiodi <matheuschiodi20@gmail.com>
2022-05-13 14:50:16 -0300

    Primeiros passos na estrutura de um componente

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 const App = () =>{
     return (
-        <h1>Hello World</h1>
+        <>
+            <h1>Minha primeira aplicação com React</h1>
+            <ul className="list-group">
+                <li>
+                   
+                </li>
+                <li>
+                    <a href="/" className="list-group-item list-group-item-action list-group-item-dark">Item 2</a>
+                </li>
+                <li>
+                    <a href="/" className="list-group-item list-group-item-action list-group-item-dark">Item 3</a>
+                </li>
+           </ul>
+        </>
     )
 }
 
